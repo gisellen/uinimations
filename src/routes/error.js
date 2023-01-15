725c4b8d85b8ab24d63128e50ff03bdc00a0c16b
Giselle Domingo <giselle.domingo.x@gmail.com>
2023-01-14 19:45:18 -0500

    Updated error page

--- src/routes/error.js
+++ src/routes/error.js
@@ -5,7 +5,7 @@ export default function ErrorPage(){
     console.error(error)
     return (
         <div id="error-page" style={style}>
-            <p>An error occured</p>
+            <h1>An error occured</h1>
             <p><small>maybe you clicked on a secret link or something</small></p>
             <a href="/"><button>back to home</button></a>
         </div>
@@ -13,6 +13,7 @@ export default function ErrorPage(){
 }
 
 const style ={
+    backgroundColor: ' background: rgb(248, 246, 238)',
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'center',
