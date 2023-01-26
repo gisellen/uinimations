81f163d675c93a936bbdacda2f11a36d8fb3cbf4
Gisellen <giselle.domingo.x@gmail.com>
2023-01-25 22:16:17 -0500

    WIP: Button1 Animation

--- src/components/buttons/Buttons.js
+++ src/components/buttons/Buttons.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import './Buttons.sass'
 
 export default function Buttons() {
   return (
-    <div>Buttons</div>
+    <div>
+      <h1>Buttons</h1>
+      <button className='button1'>button</button>
+    </div>
   )
 }
